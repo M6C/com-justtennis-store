1a7cb24c9270c678c2113fb41ca607c5bacdb145
M6C <david.roca@free.fr>
2018-08-04 09:40:35 +0200

    Add generate_keystore.cmd

---
+++ generate_keystore.cmd
@@ -0,0 +1,3 @@
+keytool -storepasswd -new R3214d3214 -keystore D:\Dev\Android\Workspace_Android_Others\com-justtennis-store\keystore.jks
+
+keytool -keypasswd -alias com.justtennis -new 3214tennis -keystore D:\Dev\Android\Workspace_Android_Others\com-justtennis-store\keystore.jks
